7620b2d71e5890056bc96a8dfbdf1712f89438f1
good_pen <5649701+llwwff@user.noreply.gitee.com>
2022-06-17 13:34:19 +0800

    准备禁用tree-sitter__debug_plenary

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,10 +3,13 @@
 HERE="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 cd $HERE/..
 
+echo '这里调用plenary    /home/wf/.local/share/nvim/PL/tree_sitter-nvim/scripts/run_tests.sh'
+
 run() {
-    nvim --headless --noplugin -u scripts/minimal_init.lua \
-        -c "PlenaryBustedDirectory $1 { minimal_init = './scripts/minimal_init.lua' }"
-}
+        echo '这里调用plenary.  准备run'
+        nvim --headless --noplugin -u scripts/minimal_init.lua \
+            -c "PlenaryBustedDirectory $1 { minimal_init = './scripts/minimal_init.lua' }"
+        }
 
 if [[ $2 = '--summary' ]]; then
     ## really simple results summary by filtering plenary busted output
